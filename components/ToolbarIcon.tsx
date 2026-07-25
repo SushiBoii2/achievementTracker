aaf23fa48a7a4e590f9ecb45b24bfbddfc2832dd
sushi <glitchycement56@gmail.com>
2026-07-25 18:48:57 +1000

    Update ToolbarIcon.tsx

--- components/ToolbarIcon.tsx
+++ components/ToolbarIcon.tsx
@@ -18,10 +18,28 @@ const HeaderBarIcon = findComponentByCodeLazy(".HEADER_BAR_BADGE_TOP:", '"aria-h
 
 function TrophyIcon() {
     return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            {/* Main fill */}
             <path
-                fill="currentColor"
                 d="M17 4V2H7v2H2v3c0 2.4 1.72 4.38 4 4.9.44 1.84 1.83 3.31 3.62 3.86L9 20H7v2h10v-2h-2l-.62-6.24A5.007 5.007 0 0 0 18 9.9c2.28-.52 4-2.5 4-4.9V4h-5ZM4 7V6h2.1c.14 1.47.55 2.85 1.18 4.09C5.83 9.6 4 8.5 4 7Zm12.72 3.09A5.99 5.99 0 0 0 17.9 6H20v1c0 1.5-1.83 2.6-3.28 3.09Z"
+                fill="currentColor"
+                opacity="0.9"
+            />
+
+            {/* Soft outline */}
+            <path
+                d="M17 4V2H7v2H2v3c0 2.4 1.72 4.38 4 4.9.44 1.84 1.83 3.31 3.62 3.86L9 20H7v2h10v-2h-2l-.62-6.24A5.007 5.007 0 0 0 18 9.9c2.28-.52 4-2.5 4-4.9V4h-5ZM4 7V6h2.1c.14 1.47.55 2.85 1.18 4.09C5.83 9.6 4 8.5 4 7Zm12.72 3.09A5.99 5.99 0 0 0 17.9 6H20v1c0 1.5-1.83 2.6-3.28 3.09Z"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="0.6"
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                opacity="0.3"
             />
         </svg>
     );
